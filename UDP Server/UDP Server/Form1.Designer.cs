c1c785dbe41080a3ccba3abd0746760f7be205e6
minyou08042 <57974447+minyou08042@users.noreply.github.com>
2021-04-26 15:42:26 +0800

    [add] UDP Server init

---
+++ UDP Server/UDP Server/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace UDP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox1.Location = new System.Drawing.Point(82, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnConnect.Location = new System.Drawing.Point(249, 18);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(103, 37);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "連接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label2.Location = new System.Drawing.Point(373, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(23, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(607, 284);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnExit.Location = new System.Drawing.Point(457, 375);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(173, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "結束程式";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnClear.Location = new System.Drawing.Point(23, 375);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(167, 35);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "清除";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 422);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ UDP Server/UDP Server/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDP_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        public void btnConnect_Click(object sender, EventArgs e)
+        {
+            int port = Convert.ToInt32(textBox1.Text);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port);
+            Server.Bind(ipep);
+            label2.Text = "已取得連線";
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.Start();
+        }
+
+        public delegate void MyInvoke(string input);
+
+        private void ThreadProc()
+        {
+            while (true)
+            {
+                IPEndPoint send = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint Remote = (EndPoint)send;
+                byte[] getdata = new byte[1024];
+                int recv = Server.ReceiveFrom(getdata, ref Remote);
+                string input = Encoding.UTF8.GetString(getdata, 0, recv);
+                MyInvoke mi = new MyInvoke(UpdateForm);
+                this.BeginInvoke(mi, new Object[] { input });
+                Server.SendTo(Encoding.UTF8.GetBytes("OK"), Remote);
+                Thread.Sleep(0);
+            }
+        }
+
+        public void UpdateForm(string input)
+        {
+            listBox1.Items.Add("已接收: " + input + Environment.NewLine);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
