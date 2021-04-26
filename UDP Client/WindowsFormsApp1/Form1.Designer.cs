6a230636f5d35d5b609f16c2486cbfa89f320098
minyou08042 <57974447+minyou08042@users.noreply.github.com>
2021-04-26 15:41:48 +0800

    [add] UDP Client init

---
+++ UDP Client/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMsgSend = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtIp.Location = new System.Drawing.Point(146, 33);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(158, 31);
+            this.txtIp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(32, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtPort.Location = new System.Drawing.Point(146, 79);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(158, 31);
+            this.txtPort.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnSend.Location = new System.Drawing.Point(648, 360);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 39);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "發送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMsgSend
+            // 
+            this.txtMsgSend.Font = new System.Drawing.Font("新細明體", 12F);
+            this.txtMsgSend.Location = new System.Drawing.Point(359, 366);
+            this.txtMsgSend.Name = "txtMsgSend";
+            this.txtMsgSend.Size = new System.Drawing.Size(269, 31);
+            this.txtMsgSend.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnConnect.Location = new System.Drawing.Point(12, 125);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(107, 36);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "連線";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label3.Location = new System.Drawing.Point(154, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 34);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnExit.Location = new System.Drawing.Point(13, 362);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(291, 35);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "結束程式";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(359, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(364, 284);
+            this.listBox1.TabIndex = 12;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("新細明體", 12F);
+            this.btnClose.Location = new System.Drawing.Point(12, 177);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 40);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "結束連接";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 421);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMsgSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMsgSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ UDP Client/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtIp.Text) == true)
+            {
+                MessageBox.Show("請輸入IP");
+            }
+            if (string.IsNullOrEmpty(txtPort.Text) == true)
+            {
+                MessageBox.Show("請輸入PORT");
+            }
+            label3.Text = "已取得連線";
+
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            t.Start();
+        }
+
+        public delegate void MyInvoke(byte[] data, int recv);
+
+        public void ThreadProc()
+        {
+            byte[] data = new byte[4096];
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
+            Client.Bind(Remote);
+            while (true)
+            {
+                int recv = Client.ReceiveFrom(data, ref Remote);
+                MyInvoke mi = new MyInvoke(UpdateForm);
+                this.BeginInvoke(mi, new Object[] { data, recv });
+                Thread.Sleep(0);
+            }
+        }
+
+        public void UpdateForm(byte[] data, int recv)
+        {
+            listBox1.Items.Add("已接收： " + Encoding.UTF8.GetString(data, 0, recv) + Environment.NewLine);
+        }
+
+        Socket Client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        public void btnSend_Click(object sender, EventArgs e)
+        {
+            string input = txtMsgSend.Text;
+            string ip = txtIp.Text;
+            if (string.IsNullOrEmpty(ip) == true)
+            {
+                MessageBox.Show("請輸入IP");
+            }
+            IPEndPoint remoteIP = new IPEndPoint(IPAddress.Parse(ip), Convert.ToInt32(txtPort.Text));
+            Client.SendTo(Encoding.UTF8.GetBytes(input), remoteIP);
+            listBox1.Items.Add("已傳送： " + Encoding.UTF8.GetString(Encoding.UTF8.GetBytes(input), 0, input.Length) + Environment.NewLine);
+            txtMsgSend.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Client.Close();
+            label3.Text = "已關閉連線";
+        }
+
+        public void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
